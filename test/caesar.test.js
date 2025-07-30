53abbba964c1d40ac945c244ec1d9d735e8c5777
Leonardo <leonardomullerdelima@pm.me>
2025-07-30 12:48:52 -0300

    feat: add caesarCipher test

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -4,4 +4,8 @@ describe("caesarCipher function", () => {
   it("Ciphers a simple string", () => {
     expect(Main.caesarCipher("abc", 5)).toBe("fgh");
   });
+
+  it("Ignores non alphabetical characters", () => {
+    expect(Main.caesarCipher("!abc?", 5)).toBe("!fgh?");
+  });
 });
